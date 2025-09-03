f47ae752074bcbd38f274cc0821b5501ba116b99
ademmenh <ademmenh777@gmail.com>
2025-09-03 01:30:08 +0100

    feat: added the terminal emulator gui, the master side

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC = gcc
-CFLAGS = -Iinclude
+CFLAGS = -Iinclude `pkg-config --cflags gtk+-3.0 vte-2.91`
+LDFLAGS = `pkg-config --libs gtk+-3.0 vte-2.91`
 SRC_DIR = zebra/
 BUILD = build/
 TARGET = $(BUILD)zebra
@@ -8,7 +9,7 @@ SRC = $(SRC_DIR)*
 all: $(TARGET)
 
 $(TARGET): $(SRC)
-	$(CC) $(CFLAGS) -o $(TARGET) $(SRC)
+	$(CC) $(CFLAGS) -o $(TARGET) $(SRC) $(LDFLAGS)
 
 clean:
 	rm -f $(TARGET)
--- zebra/main.c
+++ zebra/main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <gtk/gtk.h>
+#include <vte/vte.h>
 #include "pty.h"
 #include "debug.h"
 
-int runParent(Pty *pty){
+int runParent(Pty *pty) {
     close(pty->slave);
-    sleep(5);
+    gtk_init(NULL, NULL);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Terminal Emulator");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    GtkWidget *terminal = vte_terminal_new();
+    GError *error = NULL;
+    VtePty *vte_pty = vte_pty_new_foreign_sync(pty->master, NULL, &error);
+    if (!vte_pty) {
+        g_printerr("Failed to create VtePty: %s\n", error ? error->message : "unknown error");
+        g_clear_error(&error);
+        return 1;
+    }
+    vte_terminal_set_pty(VTE_TERMINAL(terminal), vte_pty);
+    gtk_container_add(GTK_CONTAINER(window), terminal);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_widget_show_all(window);
+    gtk_main();
     return 0;
 }
 
@@ -25,7 +43,6 @@ int runChild(Pty *pty){
         perror("ioctl");
         return 1;
     }
-    listdev();
     // redirection
     dup2(pty->slave, 0);
     dup2(pty->slave, 1);
@@ -40,7 +57,6 @@ int main (){
     if (err) return 1;
     err = get_slave(&pty);
     if (err) return 1;
-    listdev();
 
     // with fork()
     // the child process is not the pgroup leader
